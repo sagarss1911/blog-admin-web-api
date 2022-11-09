7c1b76e1427642dc9b1c92e1a4d4e6f6ed50911b
Shaista-Tanveer <shaista.tanveer@jtechworld.com>
2022-11-09 18:39:29 +0530

    added place

---
+++ modules/routes/place.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let express = require("express"),
+    router = express.Router(),
+    controller = require("../controllers/place"),
+    authMiddleware = require("../middleware/authValidation"),
+    helper = require("../helpers/fileUpload");
+//place api
+router.post('/get_all_place', authMiddleware.verifyToken, controller.getAllPlace);
+router.delete('/remove_place/:place_id', authMiddleware.verifyToken, controller.removePlace);
+router.post('/add_places', authMiddleware.verifyToken, helper.uploadPlaceImage.fields([{ name: 'image' }, { name: 'mapImage' }]), controller.addPlace);
+router.post('/get_place', authMiddleware.verifyToken, controller.getPlace);
+
+module.exports = router;
+
+
